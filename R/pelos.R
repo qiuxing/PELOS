1fe5988dcd7c5b73a000de8cd8abc2c3bb0796c8
Xing Qiu <xing.qiu@gmail.com>
2013-02-02 22:52:11 -0500

    1. make "OBSERVATION" the first argument of pelos() because this is the only argument that absolutely has no default value. 2. removed some excessive spaces.

--- R/pelos.R
+++ R/pelos.R
@@ -1,5 +1,5 @@
-pelos <- function( COEFFICIENT , INITIAL.VALUE , INTERCEPT = NULL , OBSERVATION , tol = 1e-5 , max_iter = 30 , dif_step = 1e-3 , timestep = 1e-2 , Nprint = 10 )
-#, sparse.output = FALSE )
+pelos <- function(OBSERVATION, COEFFICIENT, INITIAL.VALUE, INTERCEPT=NULL, tol=1e-5, max_iter=30, dif_step=1e-3, timestep=1e-2, Nprint=10)
+#, sparse.output=FALSE)
 {
     Nequation <- length(INITIAL.VALUE)
     TIMEPOINT <- sort(unique(OBSERVATION[,2]))
@@ -10,26 +10,26 @@ pelos <- function( COEFFICIENT , INITIAL.VALUE , INTERCEPT = NULL , OBSERVATION 
     Nnonzero <- nrow(COEFFICIENT)
     Nnonzero_intercept <- nrow(INTERCEPT)
     Nobservation <- nrow(OBSERVATION)
-    max_func <- max_iter * ( Nequation+Nnonzero+Nnonzero_intercept+3 )
+    max_func <- max_iter * (Nequation+Nnonzero+Nnonzero_intercept+3)
     Y <- numeric(Nobservation)
 
-    res <- .Fortran ( 'S_lmdif_ODE_linear' , as.integer(Nequation) , as.integer(Ntime) , as.numeric(timestep) , as.numeric(TIMEPOINT) , as.integer(Nnonzero) , as.integer(COEFFICIENT[,1]) , as.integer(COEFFICIENT[,2]) , as.integer(Nnonzero_intercept) , as.integer(INTERCEPT[,1]) , as.integer(Nobservation) , as.integer(OBSERVATION[,1]) , as.numeric(OBSERVATION[,2]) , as.numeric(OBSERVATION[,3]) , as.numeric(OBSERVATION[,4]) , as.numeric(tol) , as.integer(max_func) , as.numeric(dif_step) , as.integer(Nprint) , as.numeric(c(INITIAL.VALUE,COEFFICIENT[,3],INTERCEPT[,2])) , as.numeric(Y) )
-#    if ( sparse.output == FALSE )
+    res <- .Fortran('S_lmdif_ODE_linear', as.integer(Nequation), as.integer(Ntime), as.numeric(timestep), as.numeric(TIMEPOINT), as.integer(Nnonzero), as.integer(COEFFICIENT[,1]), as.integer(COEFFICIENT[,2]), as.integer(Nnonzero_intercept), as.integer(INTERCEPT[,1]), as.integer(Nobservation), as.integer(OBSERVATION[,1]), as.numeric(OBSERVATION[,2]), as.numeric(OBSERVATION[,3]), as.numeric(OBSERVATION[,4]), as.numeric(tol), as.integer(max_func), as.numeric(dif_step), as.integer(Nprint), as.numeric(c(INITIAL.VALUE,COEFFICIENT[,3],INTERCEPT[,2])), as.numeric(Y))
+#    if (sparse.output == FALSE)
 #    {
         PM <- matrix(0,Nequation,Nequation)
-        for ( i in 1:Nnonzero )
+        for (i in 1:Nnonzero)
         {
-            PM [ COEFFICIENT[i,1] , COEFFICIENT[i,2] ] <- res[[19]][Nequation+i]
+            PM [ COEFFICIENT[i,1], COEFFICIENT[i,2] ] <- res[[19]][Nequation+i]
         }
         IN <- numeric(Nequation)
-        for ( i in 1:Nnonzero_intercept )
+        for (i in 1:Nnonzero_intercept)
         {
             IN[INTERCEPT[i,1]] <- res[[19]][Nequation+Nnonzero+i]
         }
-        return( list( initial.value = res[[19]][1:Nequation] , coefficient.matrix = PM , intercept = IN ) )
+        return(list(initial.value=res[[19]][1:Nequation], coefficient.matrix=PM, intercept=IN))
 #    }
 #    else
 #    {
-#        return( list( initial.value = res[[17]][1:Nequation] , coefficient.matrix = res[[17]][(Nequation+1):(Nequation+Nnonzero)] , intercept = res[[17]][(Nequation+Nnonzero+1):(Nequation+Nnonzero+Nnonzero_intercept)] ) )
+#        return(list(initial.value=res[[17]][1:Nequation], coefficient.matrix=res[[17]][(Nequation+1):(Nequation+Nnonzero)], intercept=res[[17]][(Nequation+Nnonzero+1):(Nequation+Nnonzero+Nnonzero_intercept)]))
 #    }
 }
